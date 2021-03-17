2c0668e26375dc7d46e18ebbf342be00ed764bfe
Nathaniel Rindlaub <nathanielrindlaub@gmail.com>
2021-03-17 10:41:01 -0700

    Download image from linckeazi email and upload to s3

--- src/handler.js
+++ src/handler.js
@@ -2,32 +2,79 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 const simpleParser = require('mailparser').simpleParser;
+const htmlparser2 = require('htmlparser2');
+const agent = require('superagent');
 
-const EMAIL_STAGING_BUCKET = `animl-email-staging-${process.env.STAGE}`;
+const DATA_STAGING_BUCKET = `animl-data-staging-${process.env.STAGE}`;
+const LINCKEAZI_RE = new RegExp(/linckeazi/, 'i');
 
+const uploadToS3 = async (imgBuffer, key, bucket=DATA_STAGING_BUCKET) => {
+  console.log('Uploading to s3');
+  try {
+    const destparams = {
+        Bucket: bucket,
+        Key: key,
+        Body: imgBuffer,
+        ContentType: 'image'
+    };
+    const putResult = await s3.putObject(destparams).promise(); 
+    console.log('upload successful');
+    return putResult;
+  } catch (error) {
+      console.log(error);
+      return;
+  } 
+};
+
+const extractImage = {
+  'LinckEazi': async (email) => {
+    console.log('extracting image from LinckEazi email');
+    let imgLink; 
+    const parser = new htmlparser2.Parser({
+      onattribute(name, value) {
+        if (name === 'href') {
+          imgLink = value;
+        }
+      },
+    });
+    parser.write(email.html);
+    const img = await agent.get(imgLink);
+    return Buffer.from(img.body, 'binary');;
+  },
+}
+
+const extractFilename = {
+  'LinckEazi': (email) => {
+    console.log('extracting filename from LinckEazi email');
+    const subject = email.subject;
+    const parts = subject.split('_');
+    return parts.filter(part => part.includes('.jpg'))[0];
+  },
+}
 
 module.exports.relayImages = async (event) => {
+
   const record = event.Records[0];
   const request = {
     Bucket: record.s3.bucket.name,
     Key: record.s3.object.key,
   };
 
-  const filename = record.s3.object.key;
-  console.log(`New object has been detected: ${filename}`);
-  console.log('full event record: ', record);
-
   try {
     const data = await s3.getObject(request).promise();
-    console.log('Raw email:' + data.Body);
-
     const email = await simpleParser(data.Body);
+    console.log('email parsed: ', email);
 
-    console.log('date:', email.date);
-    console.log('subject:', email.subject);
-    console.log('body:', email.text);
-    console.log('from:', email.from.text);
-    console.log('attachments:', email.attachments);
+    // If email is from linckeazi
+    const make = (LINCKEAZI_RE.test(email.from.text)) ? 'LinckEazi' : 'other';
+    if (make === 'other') {
+      throw new Error('Email is not from a supported camera make');
+    }
+
+    const filename = extractFilename[make](email);
+    const imgBuffer = await extractImage[make](email);
+    await uploadToS3(imgBuffer, filename);
+
     return { status: 'success' };
   }
   catch(Error) {
